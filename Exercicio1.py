ea9385175e6fcd731b3af538f60e8fc782e8b8c8
Mércia Regina da Silva <39158160+MerciaReginasl@users.noreply.github.com>
2018-09-10 22:17:11 -0300

    Create Exercicio1.py

---
+++ Exercicio1.py
@@ -0,0 +1,47 @@
+#Exercicio 1
+
+#Questão 1
+
+import numpy as np
+M = np.arange(1,16).reshape((3,5)).T
+print(M)
+
+
+import numpy as np
+D = np.array([[2,7,12],[4,9,14]]).reshape((2,3)).T
+print(D)
+
+#Questão 2
+
+import numpy as np
+A = np.arange(10,49)
+print(A)
+
+import numpy as np
+B = np.arange(0,39)
+print(B)
+
+import numpy as np
+C = np.arange(0,20)
+print(C)
+
+#Questão 3
+
+import numpy as np
+M2 = np.arange(0,9).reshape((3,3)).T
+print(M2)
+
+#Questão 5
+
+import numpy as np
+import matplotlib.pyplot as plt
+x = np.arange(0.0, 2.0, 0.01)
+y = np.sin(2*np.pi*x)
+
+plt.plot(x,y)
+plt.title('Gráfico do Seno (Senoidal)',fontsize=20)
+
+         
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
